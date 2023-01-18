53c6f67fdd8c47b1907d23db348fbe30c6476441
ColShark <petre.marius93@gmail.com>
2023-01-18 10:54:28 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Persoana
+{
+public:
+	int inaltime;
+	double greutate;
+
+	void Merge()
+	{
+		std::cout << "- merge;" << std::endl;
+	}
+	void Doarme()
+	{
+		std::cout << "- doarme;" << std::endl;
+	}
+	void Mananca()
+	{
+		std::cout << "- mananca;" << std::endl;
+	}
+};
+
+int main()
+{
+
+	system("pause");
+}
